81b1ea50aa084c2ec2c64fab3fc7cdb927ca35aa
KranthiBhai <95521380+KranthiBhai@users.noreply.github.com>
2022-02-21 15:12:02 +0530

    created swiper and added button click

---
+++ src/Swipper.js
@@ -0,0 +1,33 @@
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y,  } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import SlideNextButton from './Component1';
+
+export default () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      // scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide><SlideNextButton /></SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
